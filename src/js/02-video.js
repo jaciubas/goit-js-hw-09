6b45b2afc2251b117c9b5b6a1516098e5fc7dac3
jaciubas <114225522+jaciubas@users.noreply.github.com>
2023-04-05 18:30:19 +0200

    commit repo

---
+++ src/js/02-video.js
@@ -0,0 +1,14 @@
+// const iframe = document.querySelector('iframe');
+import Player from '@vimeo/player';
+
+const video = document.querySelector('#vimeo-player');
+const player = new Player(video);
+const videoTime = setInterval(() => {
+  player.getCurrentTime().then(time => {
+    localStorage.setItem('currentTime', time);
+  });
+}, 1000);
+
+player
+  .setCurrentTime(localStorage.getItem('currentTime'))
+  .then(function (seconds) {});
